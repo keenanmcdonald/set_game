1dd7c2cb132e57a56e2cbd253907b8c98f3cc8a8
Keenan McDonald <keemcd@gmail.com>
2020-08-11 17:22:42 -0500

    Initial Commit

---
+++ set_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  set_game
+//
+//  Created by Keenan McDonald on 8/11/20.
+//  Copyright © 2020 Keenan McDonald. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
